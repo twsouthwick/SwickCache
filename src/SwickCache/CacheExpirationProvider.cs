a4821aa3df89cc3da6817a1039288b0f5f56e43b
Taylor Southwick <twsouthwick@outlook.com>
2020-10-26 16:42:19 -0700

    Cache method expiration

--- src/SwickCache/CacheExpirationProvider.cs
+++ src/SwickCache/CacheExpirationProvider.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Concurrent;
 using System.Reflection;
 
 namespace Swick.Cache
 {
     internal class CacheExpirationProvider
     {
+        private readonly ConcurrentDictionary<MethodInfo, DateTimeOffset?> _expirations = new ConcurrentDictionary<MethodInfo, DateTimeOffset?>();
+
         public DateTimeOffset? GetExpiration<T>(MethodInfo method, T obj)
         {
             if (obj is ICacheInvalidator invalidator)
@@ -13,19 +16,22 @@ namespace Swick.Cache
             }
             else
             {
-                var attribute = method.GetCustomAttribute<CachedAttribute>();
-
-                if (attribute is null)
+                return _expirations.GetOrAdd(method, m =>
                 {
-                    return null;
-                }
+                    var attribute = m.GetCustomAttribute<CachedAttribute>();
 
-                if (!attribute.Duration.HasValue)
-                {
-                    return null;
-                }
+                    if (attribute is null)
+                    {
+                        return null;
+                    }
 
-                return DateTimeOffset.Now.Add(attribute.Duration.Value);
+                    if (!attribute.Duration.HasValue)
+                    {
+                        return null;
+                    }
+
+                    return DateTimeOffset.Now.Add(attribute.Duration.Value);
+                });
             }
         }
     }
