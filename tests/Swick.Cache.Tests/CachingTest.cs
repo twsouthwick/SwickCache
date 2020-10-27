31b9bbc531cb5d9c11e0cc612e235e7889bcbc6f
Taylor Southwick <twsouthwick@outlook.com>
2020-10-27 11:00:20 -0700

    Add cancellation test

--- tests/Swick.Cache.Tests/CachingTest.cs
+++ tests/Swick.Cache.Tests/CachingTest.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using NSubstitute;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -20,7 +21,7 @@ namespace Swick.Cache.Tests
         }
 
         [Fact]
-        public void Sanity()
+        public void CachingManagerResolves()
         {
             // Arrange
             using var mock = AutoSubstitute.Configure()
@@ -34,13 +35,15 @@ namespace Swick.Cache.Tests
             Assert.IsNotNSubstituteMock(manager);
         }
 
+
         [Fact]
-        public async Task Sanity2()
+        public async Task SimpleAsyncMethodIsCached()
         {
             // Arrange
             var expected = _fixture.Create<string>();
             var key = _fixture.Create<string>();
             var value = _fixture.CreateMany<byte>().ToArray();
+
             using var mock = AutoSubstitute.Configure()
                 .InjectProperties()
                 .AddCaching(c =>
@@ -69,9 +72,87 @@ namespace Swick.Cache.Tests
             await mock.Resolve<IDistributedCache>().Received(1).SetAsync(key, value, Arg.Any<DistributedCacheEntryOptions>(), Arg.Any<CancellationToken>());
         }
 
+        [Fact]
+        public async Task AsyncMethodWithCancellationIsCached()
+        {
+            // Arrange
+            var expected = _fixture.Create<string>();
+            var key = _fixture.Create<string>();
+            var value = _fixture.CreateMany<byte>().ToArray();
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            using var mock = AutoSubstitute.Configure()
+                .InjectProperties()
+                .AddCaching(c =>
+                {
+                    c.CacheType<ITest>(test =>
+                    {
+                        test.Add(t => t.ReturnObjectAsyncWithCancellation(default));
+                    });
+                })
+                .MakeUnregisteredTypesPerLifetime()
+                .ConfigureService<IDistributedCache>(cache => cache.GetAsync(key, token).Returns((byte[])null))
+                .ConfigureService<ITest>(t => t.ReturnObjectAsyncWithCancellation(token).Returns(expected))
+                .SubstituteFor<ICacheKeyProvider>()
+                    .ConfigureSubstitute(t => t.GetKey(Arg.Any<MethodInfo>(), Arg.Any<object[]>()).Returns(key))
+                .ConfigureService<ICacheSerializer<object>>(t => t.GetBytes(expected).Returns((value, expected)))
+                .Build();
+
+            var cached = mock.Resolve<ICachingManager>().CreateCachedProxy(mock.Resolve<ITest>());
+
+            // Act
+            var result = await cached.ReturnObjectAsyncWithCancellation(token);
+
+            // Assert
+            Assert.Equal(expected, result);
+
+            await mock.Resolve<IDistributedCache>().Received(1).SetAsync(key, value, Arg.Any<DistributedCacheEntryOptions>(), token);
+        }
+
+        [Fact]
+        public void SimpleSyncMethodIsCached()
+        {
+            // Arrange
+            var expected = _fixture.Create<string>();
+            var key = _fixture.Create<string>();
+            var value = _fixture.CreateMany<byte>().ToArray();
+            using var mock = AutoSubstitute.Configure()
+                .InjectProperties()
+                .AddCaching(c =>
+                {
+                    c.CacheType<ITest>(test =>
+                    {
+                        test.Add(t => t.ReturnObject());
+                    });
+                })
+                .MakeUnregisteredTypesPerLifetime()
+                .ConfigureService<IDistributedCache>(cache => cache.Get(key).Returns((byte[])null))
+                .ConfigureService<ITest>(t => t.ReturnObject().Returns(expected))
+                .SubstituteFor<ICacheKeyProvider>()
+                    .ConfigureSubstitute(t => t.GetKey(Arg.Any<MethodInfo>(), Arg.Any<object[]>()).Returns(key))
+                .ConfigureService<ICacheSerializer<object>>(t => t.GetBytes(expected).Returns((value, expected)))
+                .Build();
+
+            var cached = mock.Resolve<ICachingManager>().CreateCachedProxy(mock.Resolve<ITest>());
+
+            // Act
+            var result = cached.ReturnObject();
+
+            // Assert
+            Assert.Equal(expected, result);
+
+            mock.Resolve<IDistributedCache>().Received(1).Set(key, value, Arg.Any<DistributedCacheEntryOptions>());
+        }
+
+
         public interface ITest
         {
             Task<object> ReturnObjectAsync();
+
+            Task<object> ReturnObjectAsyncWithCancellation(CancellationToken token);
+
+            object ReturnObject();
         }
     }
 }
