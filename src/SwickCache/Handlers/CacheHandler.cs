f75deb6e4bcb886632d1b799746aed5a958d0331
Taylor Southwick <twsouthwick@outlook.com>
2020-11-02 09:08:00 -0800

    Move IsDataDrained

--- src/SwickCache/Handlers/CacheHandler.cs
+++ src/SwickCache/Handlers/CacheHandler.cs
@@ -6,14 +6,14 @@ namespace Swick.Cache.Handlers
 {
     public abstract class CacheHandler<T> : CacheHandler
     {
-        protected internal virtual bool IsDataDrained => false;
-
         protected internal virtual bool ShouldCache(T obj)
             => true;
     }
 
     public abstract class CacheHandler
     {
+        protected internal virtual bool IsDataDrained => false;
+
         protected internal virtual bool ShouldCache(Type type, MethodInfo methodInfo)
             => false;
 
